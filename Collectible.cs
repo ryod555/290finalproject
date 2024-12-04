d8d4a124329e6931b2b4cfa47e96812f034c0c6f
ryo dang <143574447+ryod555@users.noreply.github.com>
2024-12-03 22:14:49 -0500

    Add files via upload

---
+++ Collectible.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject onCollectEffect;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,rotationSpeed,0);
+
+    }
+    private void OnTriggerEnter(Collider other) // Destroys collectible
+    {
+        if (other.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
+
+        // Instantiate effect
+        Instantiate(onCollectEffect, transform.position, transform.rotation);
+    }
+}
